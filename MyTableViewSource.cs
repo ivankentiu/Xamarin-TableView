3d2aa95181435b3726eb413915748c74b2baa4fe
ivantiu <ivankentiu@gmail.com>
2017-12-22 13:40:59 +0800

    Initial TableView

---
+++ MyTableViewSource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using UIKit;
+namespace listviewapp
+{
+    public class MyTableViewSource : UITableViewSource
+    {
+
+        List<string> tableItems;
+
+        public MyTableViewSource(List<string> _tableItems)
+        {
+            tableItems = _tableItems;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("tableViewCell") as MyCustomCell;
+
+            if (cell == null)
+                cell = new MyCustomCell(new NSString("tableViewCell"));
+
+            cell.UpdateCell(tableItems[indexPath.Row], tableItems[indexPath.Row]); 
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return tableItems.Count;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            tableView.DeselectRow(indexPath, true);
+        }
+
+        public override nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath)
+        {
+            return 75f;
+        }
+    }
+}
---
+++ ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace listviewapp
+{
+    public partial class ViewController : UIViewController
+    {
+        List<string> tableItems;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            tableItems = new List<string>();
+
+            for (var i = 0; i < 40; i++)
+            {
+                tableItems.Add("Item number " + i.ToString());
+            }
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            tableView.Source = new MyTableViewSource(tableItems);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
